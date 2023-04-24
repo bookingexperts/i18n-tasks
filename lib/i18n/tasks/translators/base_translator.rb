e17420c134f4aa4d4b4f6e7e5fbb48d0033b43c7
Gerjan Stokkink <gerjan@bookingexperts.nl>
2023-04-24 09:12:13 +0200

    17444 Adjust interpolation regexp to support Liquid tags
    Liquid tags are delimited by {{ }} or {% %}.
    
    Closes bookingexperts/support#17444

--- lib/i18n/tasks/translators/base_translator.rb
+++ lib/i18n/tasks/translators/base_translator.rb
@@ -130,7 +130,7 @@ module I18n::Tasks
         end
       end
 
-      INTERPOLATION_KEY_RE = /%\{[^}]+}/
+      INTERPOLATION_KEY_RE = /%\{[^\}]+\}|\{\{.*\}\}|\{%.*%\}/.freeze
 
       # @param [String] value
       # @return [String] 'hello, %{name}' => 'hello, <round-trippable string>'
