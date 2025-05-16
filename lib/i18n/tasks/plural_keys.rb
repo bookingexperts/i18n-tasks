394ce503ea78c7ff9b55ba47a9c09d6cacd627fb
Stijn Broekhuis <stijn.broekhuis@bookingexperts.com>
2025-05-16 14:05:04 +0200

    Add support for explicit `0` and `1` rules in (#635)

--- lib/i18n/tasks/plural_keys.rb
+++ lib/i18n/tasks/plural_keys.rb
@@ -6,9 +6,11 @@ module I18n::Tasks::PluralKeys
   CLDR_CATEGORY_KEYS = %w[zero one two few many other].freeze
   PLURAL_KEY_SUFFIXES = Set.new CLDR_CATEGORY_KEYS
   PLURAL_KEY_RE = /\.(?:#{CLDR_CATEGORY_KEYS * '|'})$/
+  # Ref: https://unicode-org.github.io/cldr/ldml/tr35-numbers.html#Explicit_0_1_rules
+  EXPLICIT_0_1 = %w[0 1].freeze
 
   def collapse_plural_nodes!(tree)
-    tree.leaves.map(&:parent).compact.uniq.each do |node|
+    tree.leaves.filter_map(&:parent).uniq.each do |node|
       children = node.children
       next unless plural_forms?(children)
 
@@ -23,7 +25,7 @@ module I18n::Tasks::PluralKeys
   # @param [String] locale to pull key data from
   # @return [String] the base form if the key is a specific plural form (e.g. apple for apple.many), the key otherwise.
   def depluralize_key(key, locale = base_locale)
-    return key if key !~ PLURAL_KEY_RE
+    return key unless PLURAL_KEY_RE.match?(key)
 
     key_name = last_key_part(key)
     parent_key = key[0..- (key_name.length + 2)]
@@ -56,6 +58,6 @@ module I18n::Tasks::PluralKeys
   end
 
   def plural_suffix?(key)
-    PLURAL_KEY_SUFFIXES.include?(key) || %w[0 1].include?(key)
+    PLURAL_KEY_SUFFIXES.include?(key) || EXPLICIT_0_1.include?(key)
   end
 end
