cade45c2ff44a3992879ebcda3c804eea4d7fd9c
David Wessman <david.wesmn@gmail.com>
2025-06-29 18:05:29 +0200

    Switch to StandardRB configuration for Rubocop (#646)
    - Switches to StandardRB configuration for Rubocop
    - Runs autoformat on all files
    - Regenerates rubocop todo.

--- spec/interpolations_spec.rb
+++ spec/interpolations_spec.rb
@@ -1,30 +1,31 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require "spec_helper"
 
-RSpec.describe 'Interpolations' do
+RSpec.describe "Interpolations" do
   let!(:task) { I18n::Tasks::BaseTask.new }
 
   let(:base_keys) do
-    { 'a' => 'hello %{world}', 'b' => 'foo', 'c' => { 'd' => 'hello %{name}' }, 'e' => 'ok', 'f' => '%%{escaped}',
-      'g' => 'okay' }
+    {"a" => "hello %{world}", "b" => "foo", "c" => {"d" => "hello %{name}"}, "e" => "ok", "f" => "%%{escaped}",
+     "g" => "okay"}
   end
   let(:test_keys) do
-    { 'a' => 'hello', 'b' => 'foo %{bar}', 'c' => { 'd' => 'hola %{amigo}' }, 'e' => 'ok', 'f' => 'okay',
-      'g' => '%%{ignored}' }
+    {"a" => "hello", "b" => "foo %{bar}", "c" => {"d" => "hola %{amigo}"}, "e" => "ok", "f" => "okay",
+     "g" => "%%{ignored}"}
   end
 
   around do |ex|
     TestCodebase.setup(
-      'config/i18n-tasks.yml' => { base_locale: 'en', locales: %w[es] }.to_yaml,
-      'config/locales/en.yml' => { 'en' => base_keys }.to_yaml,
-      'config/locales/es.yml' => { 'es' => test_keys }.to_yaml
+      "config/i18n-tasks.yml" => {base_locale: "en", locales: %w[es]}.to_yaml,
+      "config/locales/en.yml" => {"en" => base_keys}.to_yaml,
+      "config/locales/es.yml" => {"es" => test_keys}.to_yaml
     )
 
     TestCodebase.in_test_app_dir { ex.call }
     TestCodebase.teardown
   end
 
+<<<<<<< HEAD
   context 'when using ruby string interpolations' do
     let(:base_keys) { { 'a' => 'hello %{world}', 'b' => 'foo', 'c' => { 'd' => 'hello %{name}' }, 'e' => 'ok' } }
     let(:test_keys) { { 'a' => 'hello', 'b' => 'foo %{bar}', 'c' => { 'd' => 'hola %{amigo}' }, 'e' => 'ok' } }
@@ -83,5 +84,15 @@ RSpec.describe 'Interpolations' do
       expect(leaves[3].full_key).to eq 'es.h'
       expect(leaves[4].full_key).to eq 'es.j'
     end
+=======
+  it "#inconsistent_interpolation" do
+    wrong = task.inconsistent_interpolations
+    leaves = wrong.leaves.to_a
+
+    expect(leaves.size).to eq 3
+    expect(leaves[0].full_key).to eq "es.a"
+    expect(leaves[1].full_key).to eq "es.b"
+    expect(leaves[2].full_key).to eq "es.c.d"
+>>>>>>> 144c09d (Switch to StandardRB configuration for Rubocop (#646))
   end
 end
